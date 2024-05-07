abb6a6cd0dea124de00f8e7c90728c75c0290b2b
TechCharities <mclloydp@byui.edu>
2024-05-07 04:26:38 +0200

    Created a Job class with jobs and display and also changed the Program to run jobs. I the also created the Resume class with a list for the jobs

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+using System;
+
+// The responsibility of a Job is to hold and display personal job information.
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
