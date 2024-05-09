734836526e4e60daa2e77cdb744924d6c3e5a45e
TechCharities <mclloydp@byui.edu>
2024-05-09 02:20:15 +0200

    Created new classes  and stump code. I also changed a bit of the Program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
     }
 }
